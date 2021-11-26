8b2c2572e771482ecc74170d7b8bd76c9c0e2ca1
SullyDurgin <katsdurgin@gmail.com>
2021-11-26 15:10:59 -0500

    finch index page

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # localhost:8000/
-    path('', views.home, name="home"),
-    # localhost:8000/about/
-    path('about/', views.about, name="about"),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('finches/', views.finches_index, name='finches_index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect, render
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Cat, Toy, Photo
+
 
 # Add the following import
 from django.http import HttpResponse
@@ -12,3 +12,23 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+
+class Finch:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, classification, description, age):
+    self.name = name
+    self.classification = classification
+    self.description = description
+    self.age = age
+
+
+finches = [
+    Finch('George', 'Canary', 'loves to sing.', 2),
+    Finch('Ben', 'House Finch', 'very old.', 12),
+    Finch('Gunner', 'Hawfinch', 'Chunky.', 4),
+    Finch('Beast', 'Brambling', 'Not Friendly.', 6)
+]
+
+
+def finches_index(request):
+  return render(request, 'finches/index.html', {'finches': finches})
