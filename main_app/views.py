4cc331606bfb94ec2fe7acb8376978edf1d8e9e1
SullyDurgin <katsdurgin@gmail.com>
2021-11-26 14:33:36 -0500

    getting app set up

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Cat, Toy, Photo
 
-# Create your views here.
+# Add the following import
+from django.http import HttpResponse
+
+# Define the home view
+def home(request):
+  return HttpResponse('<h1>Home</h1>')
+
+def about(request):
+  return render(request, 'about.html')
